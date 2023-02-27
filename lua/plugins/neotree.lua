e99f3b8898462b4dabb887bdd93fb585608825cf
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-02-27 04:40:07 +0300

    change nvim-tree to neo-tree (#3)

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,61 @@
+local M = {
+  'nvim-neo-tree/neo-tree.nvim',
+  cmd = 'Neotree',
+}
+
+function M.config()
+  local config = {
+    popup_border_style = 'rounded',
+    enable_git_status = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = true,
+    use_default_mappings = false,
+    window = {
+      mappings = {
+        ['<cr>'] = 'open',
+        ['<esc>'] = 'revert_preview',
+        ['<tab>'] = { 'toggle_preview', config = { use_float = true } },
+        ['l'] = 'focus_preview',
+        ['d'] = 'delete',
+        ['r'] = 'rename',
+        ['y'] = 'copy_to_clipboard',
+        ['x'] = 'cut_to_clipboard',
+        ['p'] = 'paste_from_clipboard',
+        ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
+        ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
+        ['q'] = 'close_window',
+        ['R'] = 'refresh',
+        ['?'] = 'show_help',
+        ['H'] = 'toggle_hidden',
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_by_name = {
+          --"node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          '.DS_Store',
+          'thumbs.db',
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
+    },
+  }
+
+  require('neo-tree').setup(config)
+end
+
+return M
