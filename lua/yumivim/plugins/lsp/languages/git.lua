caa12ba8c94c7717b4cc8cb490f772fee3e6305b
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-04-07 05:54:33 +0300

    Git syntax (#6)

---
+++ lua/yumivim/plugins/lsp/languages/git.lua
@@ -0,0 +1,21 @@
+local language, server, formatter, diagnostics, dap = unpack(require 'yumivim.plugins.lsp.dsl')
+
+return language {
+  name = 'git',
+
+  server.disabled {},
+
+  formatter.disabled {},
+
+  diagnostics.null {
+    runner = diagnostics.runner.commitlint,
+    config = {
+      args = {
+        '-g',
+        '~/.config/linters/commitlint.config.js',
+      },
+    },
+  },
+
+  dap.disabled {},
+}
