cffe78aa4e73c266139da9fc3b2a5d939b26e28c
Nikolay Mokrinsky <me@mokrinsky.ru>
2023-05-27 02:06:27 +0400

    refactor(filetype): switch from filetype to editorconfig

--- lua/yumivim/config/filetypes.lua
+++ lua/yumivim/config/filetypes.lua
@@ -6,47 +6,11 @@ vim.filetype.add {
     ['.*git/config'] = 'gitconfig',
     ['${HOME}/.ssh/config.*'] = 'sshconfig',
     ['.*/.kube/config'] = 'yaml',
+    ['.*/.github/workflows/.*'] = 'yaml',
   },
   extension = {
     service = function()
       return 'systemd'
-    end,
-    lua = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.softtabstop = 2
-      return 'lua'
-    end,
-    nix = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.softtabstop = 2
-      return 'nix'
-    end,
-    yaml = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.softtabstop = 2
-      return 'yaml'
-    end,
-    go = function()
-      vim.opt_local.tabstop = 4
-      vim.opt_local.shiftwidth = 4
-      vim.opt_local.softtabstop = 0
-      vim.opt_local.expandtab = false
-      return 'go'
-    end,
-    json = function()
-      vim.opt_local.conceallevel = 0
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.softtabstop = 2
-      return 'json'
-    end,
-    xml = function()
-      vim.opt_local.shiftwidth = 4
-      vim.opt_local.softtabstop = 4
-      return 'xml'
-    end,
-    js = function()
-      vim.opt_local.shiftwidth = 2
-      return 'javascript'
     end,
   },
 }
